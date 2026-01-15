abe06c05d6deb0f9fcba0c9ed85efddb053acf1e
Thomas GLR <thomasguillier@hotmail.fr>
2026-01-15 12:58:25 +0100

    Fonctionnalité : Tâche auto pour l'aggregation des températures

--- src/main/kotlin/org/exemple/iotsolarapi/IotSolarApiApplication.kt
+++ src/main/kotlin/org/exemple/iotsolarapi/IotSolarApiApplication.kt
@@ -2,8 +2,10 @@ package org.exemple.iotsolarapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class IotSolarApiApplication
 
 fun main(args: Array<String>) {
