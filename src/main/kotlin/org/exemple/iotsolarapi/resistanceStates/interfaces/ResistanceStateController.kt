6989883ba98b4a877ce479a898fa23b21a5c1a9c
thomas guillier <thomasguillier@hotmail.fr>
2026-03-03 23:43:28 +0100

    Fonctionnalité : Récupérer ACK esp32 pour valider le changement d'état de la résistance

--- src/main/kotlin/org/exemple/iotsolarapi/resistanceStates/interfaces/ResistanceStateController.kt
+++ src/main/kotlin/org/exemple/iotsolarapi/resistanceStates/interfaces/ResistanceStateController.kt
@@ -2,19 +2,17 @@ package org.exemple.iotsolarapi.resistanceStates.interfaces
 
 import org.exemple.iotsolarapi.resistanceStates.interfaces.dto.CreateResistanceStateDto
 import org.exemple.iotsolarapi.resistanceStates.interfaces.dto.ResistanceStateDto
+import org.exemple.iotsolarapi.resistanceStates.service.ResistanceAckNotifier
 import org.exemple.iotsolarapi.resistanceStates.service.ResistanceStateService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 
 @RestController
 @RequestMapping("/resistance")
 class ResistanceStateController(
-    val resistanceStateService: ResistanceStateService
+    val resistanceStateService: ResistanceStateService,
+    val resistanceAckNotifier: ResistanceAckNotifier
 ) {
     @GetMapping
     fun getLastResistanceState(): ResistanceStateDto {
@@ -30,5 +28,12 @@ class ResistanceStateController(
     @ResponseStatus(HttpStatus.CREATED)
     suspend fun createAndSendRequestToEsp32(@RequestBody createResistanceStateDto: CreateResistanceStateDto): ResistanceStateDto {
         return resistanceStateService.createAndSendRequestToEsp32(createResistanceStateDto)
+    }
+
+    @GetMapping("/ack/stream")
+    fun streamAck(): SseEmitter {
+        val emitter = SseEmitter(10 * 60_000L) // timeout  10 * 60s = 10 minutes
+        resistanceAckNotifier.addEmitter(emitter)
+        return emitter
     }
 }
